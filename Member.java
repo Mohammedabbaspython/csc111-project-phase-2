000c32382944e45e4aa6d6d84c69d2b8af08ff10
Mohammed Abbas <mabas8899@gmail.com>
2025-11-12 20:34:28 +0300

    Declared the attributes of Member

--- Member.java
+++ Member.java
@@ -1,3 +1,20 @@
 public class Member {
+    // declaring the private attributes
+    // user info + borrowed (should never reset)
+    private int id;
+    private String name;
+    private int borrowedCount;
+    // session statistics (should reset)
+    private int numViewBorrowed;
+    private int numBorrows;
+    private int numReturns;
+    private double sessionFees;
+    // declaring the Static attributes for admin statistics
+    public static double TotalRevenue;
+    public static int TotalViewBorrowed;
+    public static int TotalBorrows;
+    public static int TotalReturns;
+
+
     
 }
